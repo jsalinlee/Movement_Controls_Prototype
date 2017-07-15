3b25323fc52ea53e694bf75d32a2be9a0f711a88
jsalinlee <jsalinlee@gmail.com>
2017-07-14 18:12:09 -0700

    Testing GitHub Desktop
    Initial Commit.

---
+++ Movement Controls Prototype/Assets/CameraBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+    public Player target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z - 10);
+	}
+}
